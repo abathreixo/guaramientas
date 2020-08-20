53b3be9339f7398c78cc91eb8d9b92c27e6b211d
Néstor Casola Jiménez <ncasolajimenez@gmail.com>
2020-08-20 16:07:47 +0100

    Change from Bower to NPM
    Change from Bower to NPM, with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,15 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.scss$/i,
+                use: [
+                  'style-loader',
+                  'css-loader',
+                  'resolve-url-loader',
+                  'sass-loader'
+                ]
+            }
         ],
     },
     resolve: {
@@ -17,7 +26,8 @@ module.exports = {
     entry: {
         content: './src/content.ts',
         background: './src/background.ts',
-        popup: './src/popup.ts'
+        popup: './src/popup.ts',
+        css: './src/css/css.ts'
     },
     output: {
         filename: '[name].js'
